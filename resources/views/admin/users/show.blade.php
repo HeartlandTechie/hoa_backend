e5ba3a95a5c603b3e2eb3e42ccf375e5d28e0cec
Laravel Daily <info@laraveldaily.com>
2022-03-11 16:59:45 +0000

    QuickAdminPanel automatic commit

--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -62,7 +62,7 @@
                             {{ trans('cruds.user.fields.address') }}
                         </th>
                         <td>
-                            {{ $user->address->address ?? '' }}
+                            {{ $user->address->street_address ?? '' }}
                         </td>
                     </tr>
                 </tbody>
